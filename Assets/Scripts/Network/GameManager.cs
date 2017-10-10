2303377cd8b171bc4ee7d9f7a9abc579e487a4f3
DBeumans <buncho.gamingnetwork@gmail.com>
2017-10-10 13:49:11 +0200

    Added basic networking

---
+++ Assets/Scripts/Network/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Photon.PunBehaviour {
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+    {
+        Debug.Log("New player connected: " + newPlayer.NickName);
+        if (!PhotonNetwork.isMasterClient)
+            return;
+
+        loadLevel();
+    }
+
+    public override void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+    {
+        Debug.Log("Player disconnected : " + otherPlayer.NickName);
+
+        if (!PhotonNetwork.isMasterClient)
+            return;
+        loadLevel();
+    }
+
+    private void loadLevel()
+    {
+        if (!PhotonNetwork.isMasterClient)
+            Debug.LogError("NOT THE MASTER CLIENT!");
+
+        Debug.Log("Loading level: " + PhotonNetwork.room.playerCount);
+        PhotonNetwork.LoadLevel("Main");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/Network/Launcher.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : Photon.PunBehaviour {
+
+    [SerializeField]
+    private PhotonLogLevel logLevel = PhotonLogLevel.Informational;
+
+    [SerializeField]
+    private byte MAX_PLAYERS;
+
+    [SerializeField]
+    private string GAME_VERSION;
+
+    [SerializeField]
+    private bool offlineMode;
+
+    private void Awake()
+    {
+        PhotonNetwork.autoJoinLobby = false;
+        PhotonNetwork.automaticallySyncScene = true;
+        PhotonNetwork.offlineMode = offlineMode;
+        PhotonNetwork.logLevel = logLevel;
+    }
+
+    public void connect()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            PhotonNetwork.ConnectUsingSettings(GAME_VERSION);
+            return;
+        }
+
+        PhotonNetwork.JoinRandomRoom();
+        
+    }
+
+    private RoomOptions roomOptions()
+    {
+        RoomOptions option;
+        option = new RoomOptions();
+        option.MaxPlayers = MAX_PLAYERS;
+        return option;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        //Failed to join room , full or none exist.
+        PhotonNetwork.CreateRoom(null, roomOptions(), null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.room.PlayerCount != 1)
+            return;
+        
+        PhotonNetwork.LoadLevel("Main");
+    }
+
+}
